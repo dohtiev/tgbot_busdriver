ab7e3befce8635ef138ecac23dd675b95ac37676
dohtiev <e.dohtiev@gmail.com>
2022-08-17 12:31:24 +0300

    data is sent to google spreadsheets

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from os.path import join, dirname
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+import pygsheets
+
+def get_from_env(key):
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    return os.environ.get(key)
 
 storage=MemoryStorage()
 
@@ -17,16 +23,10 @@ class FSMAdmin(StatesGroup):
     final = State()
     description = State()
 
-def get_from_env(key):
-    dotenv_path = join(dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
-    return os.environ.get(key)
-
 bot = Bot(token=get_from_env("TELEGRAM_BOT_TOKEN"),  parse_mode=types.ParseMode.HTML)
 driver_number = get_from_env("driver_number")
 driver_telegram = get_from_env("driver_telegram")
 dp = Dispatcher(bot, storage=storage)
-data=[]
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
@@ -75,24 +75,30 @@ async def save_description(message: types.Message, state: FSMContext):
         data['description'] = message.text
 
     async with state.proxy() as data:
-        file = open('users_data.txt', 'a')
-        for key, value in data.items():
-            file.write(f'{key}: {value}\n')
-        file.write(f'\n')
-        file.close()
+        # file = open('users_data.txt', 'a')
+        # for key, value in data.items():
+        #     file.write(f'{key}: {value}\n')
+        # file.write(f'\n')
+        # file.close()
         await message.answer(f"<i>Ваша заявка принята</i>\nИмя и фамилия: <b>{data['name']}</b>,\n"
                              f"Дата поездки: <b>{data['date']}</b>,\n"
                              f"Начальная точка: <b>{data['start']}</b>,\n"
                              f"Конечная точка: <b>{data['final']}</b>,\n"
                              f"Дополнительная информация: <b>{data['description']}</b>,\n"
                              f"<i>Спасибо за обращение</i>")
+
+    gc = pygsheets.authorize(service_file=get_from_env("path_to_credentials"))
+    sht1 = gc.open_by_key(get_from_env("sheet_key"))
+    wks = sht1.sheet1
+    data_for_sheet = [[data['name']], [data['date']], [data['start']], [data['final']], [data['description']]]
+    wks.append_table(values=data_for_sheet, start='A2', end=None, dimension='COLUMNS', overwrite=False)
+    # print(wks.get_values('A2','E2'))
     await state.finish()
 
 @dp.message_handler(Text(equals='Связаться с водителем'))
 async def get_info(message: types.Message):
     driver_info=f'Номер телефона водителя: <b>{driver_number}</b>\nТелеграмм водителя: <b>@{driver_telegram}</b>'
     await message.answer(driver_info)
-
 def main():
     executor.start_polling(dp)
 
